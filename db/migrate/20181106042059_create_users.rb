2acf6d06a1ade5c5837ad76c1cbc33df8c643b1c
Caida <9485792@gmail.com>
2018-11-05 23:56:27 -0500

    first commit

---
+++ db/migrate/20181106042059_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :session_token, null: false
+      t.string :password_digest, null: false
+      t.string :image_url, default: "https://static-cdn.jtvnw.net/user-default-pictures/0ecbb6c3-fecb-4016-8115-aa467b7c36ed-profile_image-70x70.jpg"
+
+      t.timestamps
+    end
+    add_index :users, :username
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
